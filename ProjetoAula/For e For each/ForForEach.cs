850eb83eb8b28135bbab1ecab5cae2415ac31877
Ramonm <ramonss.bque@gmail.com>
2022-03-27 13:32:03 -0300

    Estrutura for e for each

---
+++ ProjetoAula/For e For each/ForForEach.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAula.For_e_For_each
+{
+    public class ForForEach
+    {
+        public ForForEach()
+        {
+            List<string> nomes = new List<string>()
+            {
+                "Ramon",
+                "João",
+                "Henrique",
+                "Fernando",
+                "Lucas"
+            };
+
+            for (int i = 0; i < nomes.Count; i++)
+            {
+                Console.WriteLine(nomes[i]);
+            }
+
+            foreach (var nome in nomes)
+            {
+                Console.WriteLine(nome);
+            }
+
+        }
+    }
+}
