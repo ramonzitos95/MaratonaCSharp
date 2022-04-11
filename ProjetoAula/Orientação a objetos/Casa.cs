73de605f9150b62352866562b4eee836e9aa8608
Ramonm <ramonss.bque@gmail.com>
2022-04-11 20:46:32 -0300

    Iniciado OO

---
+++ ProjetoAula/Orientação a objetos/Casa.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAula
+{
+    public class Casa
+    {
+        public int Tamanho { get; set; }
+        public string Cor { get; set; }
+        public int QuantidadeDePortas { get; set; }
+        public int QuantidadeDeJanelas { get; set; }
+        public decimal ValorCasa { get; set; }
+        public bool CasaAlugada { get; set; }
+
+        public void VenderCasa()
+        {
+            Console.WriteLine("A casa foi vendida");
+        }
+
+        public void AlugarCasa()
+        {
+            Console.WriteLine("A casa foi alugada");
+        }
+
+        
+
+    }
+}
--- ProjetoAula/Program.cs
+++ ProjetoAula/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using ProjetoAula;
+
+var casaNova = new Casa();
+casaNova.Tamanho = 100;
+casaNova.ValorCasa = 250.000M;
+casaNova.Cor = "Azul";
+casaNova.CasaAlugada = true;
+
+if(casaNova.CasaAlugada)
+    casaNova.AlugarCasa();
+else
+    casaNova.VenderCasa();
