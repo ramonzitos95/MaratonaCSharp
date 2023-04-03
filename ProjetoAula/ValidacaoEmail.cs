a8cebce18935f1fbcbeb8fd0902c3f93698b5589
Ramon Silva <ramonss.bque@gmail.com>
2023-04-03 19:10:03 -0300

    Criado arquivo de validação de email

---
+++ ProjetoAula/ValidacaoEmail.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Mail;
+
+Console.WriteLine("Informe seu email:");
+var email = Console.ReadLine();
+
+try
+{
+    MailAddress mailAddress;
+
+    if (!string.IsNullOrEmpty(email))
+    {
+
+        mailAddress = new MailAddress(email);
+        Console.WriteLine($"Email valido {email}");
+    }
+}
+catch (Exception)
+{
+    Console.WriteLine($"Email inválido {email}");
+}
