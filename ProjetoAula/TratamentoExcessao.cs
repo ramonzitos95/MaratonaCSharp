23d60c71696ccc8eb3477400f5c728e5a661b668
Ramon Silva <ramonss.bque@gmail.com>
2023-04-03 19:25:54 -0300

    Criado arquivo para tratamento de excessão com C#

---
+++ ProjetoAula/TratamentoExcessao.cs
@@ -0,0 +1,19 @@
+﻿
+try
+{
+	string nome = "";
+	if (string.IsNullOrEmpty(nome))
+		throw new Exception("O nome está vazio");
+
+	Console.WriteLine("Não deu erro");
+}
+catch (Exception ex)
+{
+	Console.WriteLine(ex.Message);
+}
+finally
+{
+	Console.Write("Finalizou a conexão");
+}
+
+
